1a63002701a34ffbaa5270775c5c06321316fbda
ronnyhdez <ronny.hernandezm@gmail.com>
2024-03-17 15:04:51 -0600

    Ref #2 fleet moving

--- alien_invasion.py
+++ alien_invasion.py
@@ -54,6 +54,19 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropiately if any aliens have reached an angle"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.flee_drop_speed
+        self.settings.fleet_direction *= -1
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -109,6 +122,7 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_screen(self):
